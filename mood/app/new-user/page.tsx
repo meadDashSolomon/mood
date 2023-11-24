1f763f58fcc62a63e121dca44fdb5ff36e59db28
Mead Solomon <mead93@gmail.com>
2023-11-24 15:44:04 -0600

    implement new user auth

--- mood/app/new-user/page.tsx
+++ mood/app/new-user/page.tsx
@@ -1,5 +1,32 @@
-const NewUser = () => {
-  return <div className="">Hi</div>;
+import { prisma } from "@/utils/db";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+redirect;
+
+const createNewUser = async () => {
+  const user = await currentUser();
+
+  const match = await prisma.user.findUnique({
+    where: {
+      clerkId: user.id as string,
+    },
+  });
+
+  if (!match) {
+    await prisma.user.create({
+      data: {
+        clerkId: user.id,
+        email: user?.emailAddresses[0].emailAddress,
+      },
+    });
+  }
+
+  redirect("/journal");
+};
+
+const NewUser = async () => {
+  await createNewUser();
+  return <div className=""> . . . loading</div>;
 };
 
 export default NewUser;
--- mood/app/page.tsx
+++ mood/app/page.tsx
@@ -1,7 +1,12 @@
+import { auth } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
+auth;
 
-export default function Home() {
+export default async function Home() {
+  const { userId } = await auth();
+  let href = userId ? "/journal" : "/new-user";
+
   return (
     <div className="w-screen h-screen bg-black flex justify-center items-center text-white">
       <div className="w-full max-w-[600px] mx-auto">
@@ -10,7 +15,7 @@ export default function Home() {
           This is the best app for tracking your mood IF you're honest
         </p>
         <div className="">
-          <Link href="/journal">
+          <Link href={href}>
             <button className="bg-blue-600 px-4 py-2 rounded-lg text-xl">
               get started
             </button>
