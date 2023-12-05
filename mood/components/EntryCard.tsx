80b0d9fd8e4fae09d240156493db37944e8eacec
Mead Solomon <mead93@gmail.com>
2023-12-04 18:00:46 -0600

    setup entryCard component UI and link to the journal entry

--- mood/components/EntryCard.tsx
+++ mood/components/EntryCard.tsx
@@ -1,5 +1,12 @@
 const EntryCard = ({ entry }) => {
-  return <div className="">{entry.id}</div>;
+  const date = new Date(entry.createdAt).toDateString();
+  return (
+    <div className="divide-y divide-gray-200 overflow-h bg-white shadow">
+      <div className="px-4 py-5">{date}</div>
+      <div className="px-4 py-5">summary</div>
+      <div className="px-4 py-5">mood</div>
+    </div>
+  );
 };
 
 export default EntryCard;
