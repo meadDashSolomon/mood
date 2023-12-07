43dc71619a2991198734a7b0010c0d72b252334c
Mead Solomon <mead93@gmail.com>
2023-12-07 13:33:57 -0600

    implement qa function for vector similarity searches

--- mood/utils/ai.ts
+++ mood/utils/ai.ts
@@ -3,6 +3,10 @@ import { StructuredOutputParser } from "langchain/output_parsers";
 import z from "zod";
 import { PromptTemplate } from "langchain/prompts";
 import { Prompt } from "next/font/google";
+import { Document } from "langchain/document";
+import { loadQARefineChain } from "langchain/chains";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { MemoryVectorStore } from "langchain/vectorstores/memory";
 
 const parser = StructuredOutputParser.fromZodSchema(
   z.object({
@@ -52,4 +56,25 @@ export const analyze = async (content) => {
   } catch (e) {
     console.log(e);
   }
+};
+
+const qa = async (question, entries) => {
+  const docs = entries.map((entry) => {
+    return new Document({
+      pageContent: entry.content,
+      metadata: { id: entry.id, createdAt: entry.createdAt },
+    });
+  });
+
+  const model = new OpenAI({ temperature: 0, modelName: "gpt-3.5-turbo" });
+  const chain = loadQARefineChain(model);
+  const embeddings = new OpenAIEmbeddings();
+  const store = await MemoryVectorStore.fromDocuments(docs, embeddings);
+  const relevantDocs = await store.similaritySearch(question);
+  const res = await chain.call({
+    input_document: relevantDocs,
+    question,
+  });
+
+  return res.output_text;
 };
